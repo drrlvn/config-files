360de38dc54ed05ce991d08800e3aba7b4b6f9d0
Dror Levin <spatz@psybear.com>
2011-04-08 22:32:28 +0300

    zsh: add install script

---
+++ zsh/install.zsh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+
+if [[ -e ~/.zshrc ]]; then
+	if [[ $1 = -f ]]; then
+		shift
+	else
+		echo "Aborting: ~/.zshrc exists. Use -f to override."
+		exit 1
+	fi
+fi
+
+REPO_PATH="${1:-~/config-files}"
+
+echo -n "Using ${REPO_PATH} -- Press enter to continue..."; read
+
+> ~/.zshrc <<END
+fpath=(~/.zsh \$fpath)
+source ${REPO_PATH}/zsh/zshrc
+END
+
+mkdir -p ~/.zsh
+ln -s ${REPO_PATH}/zsh/prompt_spatz_setup ~/.zsh/
