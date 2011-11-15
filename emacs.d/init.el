9a3a7ff0f1caa9e9b36f4fed9fb810e1b88fad19
Dror Levin <spatz@psybear.com>
2011-11-15 11:14:57 +0200

    emacs: remove auto-complete package

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -205,13 +205,6 @@
 (yas/load-directory "~/.emacs.d/packages/yasnippet/snippets")
 (setq yas/indent-line 'fixed)
 
-; auto-complete
-(add-to-list 'load-path "~/.emacs.d/packages/auto-complete")
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/packages/auto-complete/ac-dict")
-(ac-config-default)
-(setq ac-dwim t)
-
 ; zencoding
 (add-to-list 'load-path "~/.emacs.d/packages/zencoding")
 (autoload 'zencoding-mode "zencoding-mode" nil t)
