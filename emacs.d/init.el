cfa82281e0a09acf1f1467ed8a7884e2f8a6957e
Dror Levin <spatz@psybear.com>
2011-11-20 16:48:28 +0200

    emacs: increase recentf-max-saved-items

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -120,7 +120,7 @@
 ; recentf and ido-recentf
 (require 'recentf)
 (recentf-mode t)
-(setq recentf-max-saved-items 50)
+(setq recentf-max-saved-items 250)
 (defun ido-recentf-open ()
   "Use `ido-completing-read' to \\[find-file] a recent file"
   (interactive)
