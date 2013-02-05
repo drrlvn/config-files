a3d98774eebdf2f45cfad16705650c76866ae32e
Dror Levin <spatz@psybear.com>
2013-02-05 17:42:36 +0200

    emacs: remove unneeded script

--- emacs.d/compile-files.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-for file in **/*.el; do
-    if [[ ! -e ${file}c ]] || [[ ${file} -nt ${file}c ]]; then
-        echo -n ${file}...
-        emacsclient -a "" -e "(byte-compile-file \"$file\")" | grep -v '^t$'
-        echo " Done"
-    fi
-done
