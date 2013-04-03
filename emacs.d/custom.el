5707162071f3fafe212205b5b348a34253e0e97c
Dror Levin <spatz@psybear.com>
2013-04-03 13:57:16 +0300

    Update submodules

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -77,4 +77,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(hl-line ((t nil))))
+ )
