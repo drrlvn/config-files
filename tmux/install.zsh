4a6aa617a2dfce25f002ccb2ee95937203014933
Dror Levin <spatz@psybear.com>
2016-03-14 23:20:20 +0200

    tmux: use tpm with some plugins

---
+++ tmux/install.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/zsh
+
+REPO_PATH="${1:-~/config-files}"
+
+echo -n "Using ${REPO_PATH} -- Press enter to continue..."; read
+
+> ~/.tmux.conf <<END
+source ${REPO_PATH}/tmux/tmux.conf
+END
