2b69fb3630cd861d4d2c5864676b10b67a0fc40e
Dror Levin <spatz@psybear.com>
2016-11-20 23:10:48 +0200

    git: add install script

---
+++ install-git.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/zsh
+
+REPO_PATH="${1:-~/config-files}"
+
+echo -n "Using ${REPO_PATH} -- Press enter to continue..."; read
+
+mkdir -p ~/.config/git
+ln -sf ${REPO_PATH}/gitconfig ~/.config/git/config
