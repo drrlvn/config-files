951143f1bd67387ee96e121ede9aac56624247db
Dror Levin <spatz@psybear.com>
2017-08-27 15:42:38 +0300

    git: user include instead of symlink, remove name

--- install-git.zsh
+++ install-git.zsh
@@ -5,4 +5,7 @@ REPO_PATH="${1:-${HOME}/config-files}"
 echo -n "Using ${REPO_PATH} -- Press enter to continue..."; read
 
 mkdir -p ~/.config/git
-ln -sf ${REPO_PATH}/gitconfig ~/.config/git/config
+> ~/.config/git/config <<END
+[include]
+    path = ${REPO_PATH}/gitconfig
+END
