108199ad34219b4d68856858f5da164321f246b5
Dror Levin <spatz@psybear.com>
2017-09-12 14:09:24 +0300

    emacs: set calendar weekend to Friday and Saturday

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -165,6 +165,10 @@
 (use-package saveplace
   :config (save-place-mode 1))
 
+(use-package calendar
+  :defer
+  :config (setq calendar-weekend-days '(5 6)))
+
 (use-package compile
   :defer
   :config
