a5f523c15ae042f34de5a252d1a5e532e9f07ed2
Roey Darwish Dror <roey.ghost@gmail.com>
2018-07-05 14:31:00 +0300

    fish: override BSD utilities with GNU utilities on macos

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -1,4 +1,4 @@
 set -U fish_user_paths
-for path in ~/.gem/ruby/*/bin ~/.cargo/bin ~/bin
+for path in /usr/local/opt/coreutils/libexec/gnubin ~/.gem/ruby/*/bin ~/.cargo/bin ~/bin
     test -d "$path"; and set -U fish_user_paths "$path" $fish_user_paths
 end
